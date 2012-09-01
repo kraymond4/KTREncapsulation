9135c356e17f49d9c680d9af3a524a77f5286a11
Owner <Owner@Owner-PC>
2012-09-01 11:46:24 -0500

    Bare skeleton completed, just need methods and properties.

---
+++ src/lab2/StartUp.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author Kyle Raymond
+ */
+public class StartUp 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
